ac1a78d1cf59f4d979173407f23fa8460a4bb5f2
fortyfiveshadows <raayan.dhar@gmail.com>
2023-08-27 13:13:25 -0700

    adding a lot of image

--- script.js
+++ script.js
@@ -2,9 +2,25 @@
 const imageUrls = [
     "image1.jpg",
     "image2.jpg",
-    "image3.jpg"
-    // Add more image URLs as needed
+    "image3.jpg",
+    "image4.jpg",
+    "image5.jpg",
+    "image6.jpg",
+    "image7.jpg",
+    "image8.jpg",
+    "image9.jpg",
+    "image10.jpg",
+    "image11.jpg",
+    "image12.jpg",
+    "image13.jpg",
+    "image14.jpg",
+    "image15.jpg",
+    "image16.jpg",
+    "image17.jpg",
+    "image18.jpg",
+    "image19.jpg"
 ];
+
 
 // Get the profile image element
 const profileImg = document.getElementById("profile-img");
